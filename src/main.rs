0151bd09e39caf36e7ba7dd93175e998a4ef1789
kujenga <ataylor0123@gmail.com>
2017-11-19 16:04:19 -0500

    add cli parsing with clap for parsing input URL

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 extern crate futures;
 extern crate hyper;
 extern crate tokio_core;
+extern crate clap;
 
 use std::io::{self, Write};
 use futures::{Future, Stream};
 use hyper::Client;
 use tokio_core::reactor::Core;
+use clap::{Arg, App};
 
 fn main() {
+    let matches = App::new("rurl")
+        .about("A simple alternative to curl, written in Rust.")
+        .arg(
+            Arg::with_name("URL")
+                .help("URL to make a request to.")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let uri_str = matches.value_of("URL").unwrap();
+
     let mut core = match Core::new() {
         Ok(core) => core,
         Err(err) => panic!("Whoops: {:?}", err),
     };
     let client = Client::new(&core.handle());
-    let uri = match "http://httpbin.org/ip".parse() {
+    let uri = match uri_str.parse() {
         Ok(uri) => uri,
         Err(err) => panic!("Whoops: {:?}", err),
     };
@@ -24,5 +38,5 @@ fn main() {
             io::stdout().write_all(&chunk).map_err(From::from)
         })
     });
-    core.run(work);
+    core.run(work).unwrap();
 }
